6a47c75dafd02149098f2f8054cf28371df49c03
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-02-07 16:52:56 -0500

    Dark mode functionality added
    Inverts SVGs, manipulates root variables via Javascript.

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,18 @@
 "use strict"
 
+const root = document.querySelector(':root');
 const darkModeButton = document.getElementById('dark-mode');
+const darkModeSVGs = document.getElementsByClassName('filter-dark-mode');
+
 darkModeButton.addEventListener('click', () => {
     console.log('Dark Mode enabled');
+    root.style.setProperty('--backdrop-color', '#475569');
+    root.style.setProperty('--accent-color', '#94a3b8');
+    root.style.setProperty('--primary-color', '#64748b');
+    root.style.setProperty('--shading-color', '#1e293b');
+    root.style.setProperty('--subtext-color', 'lightgrey');
+    root.style.setProperty('--text-color', 'white');
+    for (let i = 0; i < darkModeSVGs.length; i++) {
+        darkModeSVGs[i].classList.add('invert');
+    }
 })
