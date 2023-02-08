52e86117ca3118b10893bfcc5cac783779ed2ee5
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-02-08 14:01:39 -0500

    Added sidebar collapse/expand functionality

--- dashboard.js
+++ dashboard.js
@@ -4,6 +4,10 @@ const root = document.querySelector(':root');
 const darkModeButton = document.getElementById('dark-mode');
 const darkModeSVGs = document.getElementsByClassName('filter-dark-mode');
 
+const sidebarExpandButton = document.getElementById('side-bar-expand');
+const sidebar = document.getElementById('side-bar');
+const body = document.body;
+
 darkModeButton.addEventListener('click', () => {
     if (!(root.style.getPropertyValue('--backdrop-color') === '#475569')) {
         console.log('Dark Mode enabled');
@@ -27,5 +31,17 @@ darkModeButton.addEventListener('click', () => {
         for (let i = 0; i < darkModeSVGs.length; i++) {
             darkModeSVGs[i].classList.remove('invert');
         }
+    }
+})
+
+sidebarExpandButton.addEventListener('click', () => {
+    if (!(body.classList.contains('grid-collapse'))) {
+        console.log('sidebar collapsed');
+        body.classList.add('grid-collapse');
+        sidebar.classList.add('side-bar-collapse');
+    } else {
+        console.log('sidebar expanded');
+        body.classList.remove('grid-collapse');
+        sidebar.classList.remove('side-bar-collapse');
     }
 })
