e4d47af94627e0d9c42e66169957852edc53cb60
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-02-09 15:50:36 -0500

    Made dark mode into theme buttons
    Theme of the page can now be selected from 6 palettes

--- dashboard.js
+++ dashboard.js
@@ -8,10 +8,27 @@ const sidebarExpandButton = document.getElementById('side-bar-expand');
 const sidebar = document.getElementById('side-bar');
 const body = document.body;
 
+const blocker = document.getElementById('blocker');
+const themesBar = document.getElementById('theme-selector');
+const themesButtons = document.getElementsByName('theme');
+
 darkModeButton.addEventListener('click', () => {
-    root.classList.toggle('dark-mode');
-    for (let i = 0; i < darkModeSVGs.length; i++) {
-        darkModeSVGs[i].classList.toggle('invert');
+    blocker.classList.remove('hide-themes-bar');
+})
+
+themesBar.addEventListener('change', (e) => {
+    console.log(`${e.target.value}`);
+    root.className = '';
+    root.classList.add(`${e.target.value}`);
+    blocker.classList.add('hide-themes-bar')
+    if (!(e.target.value == 'default')) {
+        for (let i = 0; i < darkModeSVGs.length; i++) {
+            darkModeSVGs[i].classList.add('invert');
+        }
+    } else {
+        for (let i = 0; i < darkModeSVGs.length; i++) {
+            darkModeSVGs[i].classList.remove('invert');
+        }
     }
 })
 
@@ -19,4 +36,8 @@ sidebarExpandButton.addEventListener('click', () => {
     console.log('sidebar collapsed');
     body.classList.toggle('grid-collapse');
     sidebar.classList.toggle('side-bar-collapse');
+})
+
+blocker.addEventListener('click', () => {
+    blocker.classList.add('hide-themes-bar');
 })
