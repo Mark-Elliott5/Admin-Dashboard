26bccef8e5366e3826ab0fd78d2bf843824dfd76
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-02-09 12:27:07 -0500

    Revised dark mode toggle code, added README
    I forgot classList.toggle was a thing.

---
+++ README.md
@@ -0,0 +1,2 @@
+Site is currently deployed [here](https://mark-elliott5.github.io/Admin-Dashboard/).
+![Admin Dashboard](images/example.png)
--- dashboard.js
+++ dashboard.js
@@ -9,28 +9,9 @@ const sidebar = document.getElementById('side-bar');
 const body = document.body;
 
 darkModeButton.addEventListener('click', () => {
-    if (!(root.style.getPropertyValue('--backdrop-color') === '#475569')) {
-        console.log('Dark Mode enabled');
-        root.style.setProperty('--backdrop-color', '#475569');
-        root.style.setProperty('--accent-color', '#94a3b8');
-        root.style.setProperty('--primary-color', '#64748b');
-        root.style.setProperty('--shading-color', '#1e293b');
-        root.style.setProperty('--subtext-color', 'lightgrey');
-        root.style.setProperty('--text-color', 'white');
-        for (let i = 0; i < darkModeSVGs.length; i++) {
-            darkModeSVGs[i].classList.add('invert');
-        }
-    } else {
-        console.log('Dark Mode disabled');
-        root.style.setProperty('--backdrop-color', 'white');
-        root.style.setProperty('--accent-color', '#f0b429');
-        root.style.setProperty('--primary-color', '#1992d4');
-        root.style.setProperty('--shading-color', '#e2e8f0');
-        root.style.setProperty('--subtext-color', 'grey');
-        root.style.setProperty('--text-color', 'black');
-        for (let i = 0; i < darkModeSVGs.length; i++) {
-            darkModeSVGs[i].classList.remove('invert');
-        }
+    root.classList.toggle('dark-mode');
+    for (let i = 0; i < darkModeSVGs.length; i++) {
+        darkModeSVGs[i].classList.toggle('invert');
     }
 })
 
